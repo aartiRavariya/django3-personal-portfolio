d91df56a936facf8b98682f55afd321df55a875b
Aarti Ravariya <patelaiysha.py@gmail.com>
2020-05-19 23:31:05 +0530

    First commit!

---
+++ portfolio/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Project(models.Model):
+    title =  models.CharField(max_length=100)
+    language = models.CharField(max_length=50)
+    description = models.CharField(max_length=250)
+    image = models.ImageField(upload_to='portfolio/images/')
+    url = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.title
+
